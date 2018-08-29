ea04bac5f37dc8a57081a1ef77195fd434f7dd42
User <User@WIN-GQOKRARBRRP>
2018-08-29 16:49:45 +0300

    Write an application that handles a student report card. The application shall save the marks of computer science, math and english. Each subject has a total of 50 marks. If a student obtains at least 75/150 marks, an event will trigger and show a congratulation message on passing the exam. Otherwise, it will display an “F” grade.

---
+++ Challenge 7-Student Report Card Application/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_7_Student_Report_Card_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            ReportCard report = new ReportCard();
+            report.Result += ReportOnResult;
+            Console.WriteLine("Enter you marks: ");
+            Console.Write("Computer science: ");
+            Int32.TryParse(Console.ReadLine(), out input);
+            report.ComputerScience = input;
+            Console.WriteLine();
+
+            Console.Write("Math: ");
+            Int32.TryParse(Console.ReadLine(), out input);
+            report.Math = input;
+            Console.WriteLine();
+
+            Console.Write("English: ");
+            Int32.TryParse(Console.ReadLine(), out input);
+            Console.WriteLine();
+            report.English = input;
+        }
+
+        private static void ReportOnResult(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            if (((ReportCard) sender).Sum >= 75)
+            {
+                Console.WriteLine(
+                    $"Congratulations!!! You've been passed an exam! Your score: {((ReportCard) sender).Sum}/150");
+            }
+            else
+            {
+                Console.WriteLine(
+                    $"Don't worry! Try again later! Your score: {((ReportCard)sender).Sum}/150");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Challenge 7-Student Report Card Application/ReportCard.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Challenge_7_Student_Report_Card_Application
+{
+    public class ReportCard
+    {
+        private int _computerScience;
+        private int _math;
+        private int _english;
+        public event PropertyChangedEventHandler Result;
+
+        public ReportCard(int computerScience = -1, int math = -1, int english = -1)
+        {
+            _computerScience = computerScience;
+            _math = math;
+            _english = english;
+        }
+
+        public int Sum
+        {
+            get => _computerScience + _math + _english;
+        }
+
+        public int ComputerScience
+        {
+            get { return _computerScience; }
+            set
+            {
+                if (value <= 50 && value >= 0)
+                {
+                    _computerScience = value;
+                    Check();
+                }
+            }
+        }
+
+        public int Math
+        {
+            get { return _math; }
+            set
+            {
+                if (value <= 50 && value >= 0)
+                {
+                    _math = value;
+                    Check();
+                }
+            }
+        }
+
+        public int English
+        {
+            get { return _english; }
+            set
+            {
+                if (value <= 50 && value >= 0)
+                {
+                    _english = value;
+                    Check();
+                }
+            }
+        }
+
+        private void Check()
+        {
+            if (ComputerScience > 0 && Math > 0 && English > 0)
+            {
+                Result.Invoke(this, new PropertyChangedEventArgs(Sum.ToString()));
+            }
+        }
+    }
+}
