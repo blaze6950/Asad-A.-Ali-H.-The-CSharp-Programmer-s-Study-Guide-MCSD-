bde1833b2e1f8cf1020dcdaee4f331331678fe69
User <User@WIN-GQOKRARBRRP>
2018-08-29 16:59:04 +0300

    Create a class Person which has a property “Name”. Your task is to invoke an event that checks if a person’s name has changed or not and assign a new value to a person name.

---
+++ Challenge 8-Invoke an event if a person’s name is changed/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Challenge_8_Invoke_an_event_if_a_person_s_name_is_changed
+{
+    public class Person
+    {
+        private string _name;
+
+        public Person()
+        {
+            NameChanged += OnNameChanged;
+        }
+
+        private void OnNameChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            if (propertyChangedEventArgs.PropertyName.Equals(_name))
+            {
+                Console.WriteLine("The name has not been changed");
+            }
+            else
+            {
+                Console.WriteLine("The name has been changed!");
+                _name = propertyChangedEventArgs.PropertyName;
+            }
+        }
+
+        public event PropertyChangedEventHandler NameChanged;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                NameChanged.Invoke(this, new PropertyChangedEventArgs(value));
+            }
+        }
+    }
+}
---
+++ Challenge 8-Invoke an event if a person’s name is changed/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_8_Invoke_an_event_if_a_person_s_name_is_changed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person newPerson = new Person();
+            newPerson.Name = "Nikita";
+            newPerson.Name = "Nikita";
+            newPerson.Name = "Alesha";
+        }
+    }
+}
