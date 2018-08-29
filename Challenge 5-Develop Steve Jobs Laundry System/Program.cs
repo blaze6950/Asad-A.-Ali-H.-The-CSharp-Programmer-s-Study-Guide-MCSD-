68ac1ef30d46876c3ea1f99b098ac775a2042c01
User <User@WIN-GQOKRARBRRP>
2018-08-29 17:02:55 +0300

    Small fixes at 5 challenge

--- Challenge 5-Develop Steve Jobs Laundry System/Program.cs
+++ Challenge 5-Develop Steve Jobs Laundry System/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Challenge_5_Develop_Steve_Jobs_Laundry_System
 {
@@ -10,14 +7,14 @@ namespace Challenge_5_Develop_Steve_Jobs_Laundry_System
     {
         private static LaundryObj _laundryObj;
 
-        static void Main(string[] args)
+        static void Main()
         {
-            List<Clothes> _mineClothes = new List<Clothes>();
+            List<Clothes> mineClothes = new List<Clothes>();
             for (int i = 0; i < 10; i++)
             {
-                _mineClothes.Add(new Clothes("T-Shirt", DateTime.Now - TimeSpan.FromDays(i + 4)));
+                mineClothes.Add(new Clothes("T-Shirt", DateTime.Now - TimeSpan.FromDays(i + 4)));
             }
-            _laundryObj = new LaundryObj(_mineClothes);
+            _laundryObj = new LaundryObj(mineClothes);
             _laundryObj.GetClothesLaundered();
         }
     }
