15682e83a4b01bacd2b0169a2e39953bfa8d7b12
User <User@WIN-GQOKRARBRRP>
2018-08-30 15:46:13 +0300

    Write an application that gets the html code of www.google.com and print the html code on the console screen. You have to control the lifetime of unmanaged resources and ensure that they are disposed properly by using Disposable Pattern.

---
+++ Challenge 10-Print Html Code of google.com/HTMLViewer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+
+namespace Challenge_10_Print_Html_Code_of_google.com
+{
+    public class HtmlViewer : IDisposable
+    {
+        private Uri _url;
+        private string _html;
+        private WebClient _webClient;
+
+        public HtmlViewer(string url)
+        {
+            _url = new Uri(url);
+        }
+
+        public Uri Url
+        {
+            get { return _url; }
+            set
+            {
+                _url = value;
+            }
+        }
+
+        public void Show()
+        {
+            Load();
+            Print();
+        }
+
+        private void Print()
+        {
+            Console.WriteLine(_html);
+        }
+
+        private void Load()
+        {
+            if (_url != null)
+            {
+                _webClient = new WebClient();
+                _html = _webClient.DownloadString(_url);
+            }
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _webClient?.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~HtmlViewer()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ Challenge 10-Print Html Code of google.com/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Challenge_10_Print_Html_Code_of_google.com
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (HtmlViewer viewer = new HtmlViewer(@"http://www.google.com"))
+            {
+                viewer.Show();
+            }
+        }
+    }
+}
