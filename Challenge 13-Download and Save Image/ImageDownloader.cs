3d81fa1ed77f54e6a411ef66f934ae42a69d994d
User <User@WIN-GQOKRARBRRP>
2018-09-03 10:57:31 +0300

    Download any of Image and convert in bytes and save those bytes in file named ImageData in your local space. Read those bytes from ImageData file and convert them into Image form and save obtained image in local drive as well.

---
+++ Challenge 13-Download and Save Image/ImageDownloader.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+
+namespace Challenge_13_Download_and_Save_Image
+{
+    public class ImageDownloader
+    {
+        private string _url;
+        private string _path = @"E:\GoogleDrive\Программовня\Work\FirstProgram\Challenge 13-Download and Save Image\image.png";
+
+        public void SaveImage(string url)
+        {
+            _url = url;
+            GetImage();
+            WriteToFileByte();
+            ReadFromFileByte();
+        }
+
+        private void WriteToFileByte()
+        {
+            var bytes = File.ReadAllBytes(_path);
+            ByteArrayToFile(_path.Substring(0, _path.Length - 9) + "ImageData.bin", bytes);
+        }
+
+        public bool ByteArrayToFile(string fileName, byte[] byteArray)
+        {
+            try
+            {
+                using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.Read))
+                {
+                    fs.Write(byteArray, 0, byteArray.Length);
+                    fs.Close();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in process: {0}", ex);
+                return false;
+            }
+        }
+
+        private void ReadFromFileByte()
+        {
+            try
+            {
+                using (var fs = new FileStream(_path.Substring(0, _path.Length - 9) + "ImageData.bin", FileMode.Open, FileAccess.Read))
+                {
+                    Image image = Image.FromStream(fs);
+                    image.Save(_path);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in process: {0}", ex);
+            }
+        }
+
+        private void GetImage()
+        {
+            var image = DownloadImageFromUrl(_url.Trim());
+            image.Save(_path);
+        }
+
+        public Image DownloadImageFromUrl(string imageUrl)
+        {
+            Image image;
+            WebResponse webResponse = null;
+
+            try
+            {
+                var webRequest =
+                    (HttpWebRequest) WebRequest.Create(imageUrl);
+                webRequest.AllowWriteStreamBuffering = true;
+                webRequest.Timeout = 30000;
+
+                webResponse = webRequest.GetResponse();
+                var stream = webResponse.GetResponseStream();
+                image = Image.FromStream(stream ?? throw new InvalidOperationException());
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                if (webResponse != null) webResponse.Close();
+            }
+
+            return image;
+        }
+    }
+}
---
+++ Challenge 13-Download and Save Image/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Challenge_13_Download_and_Save_Image
+{
+    class Program
+    {
+        static void Main()
+        {
+            ImageDownloader neDownloader = new ImageDownloader();
+            neDownloader.SaveImage(@"https://www.bmw.ua/content/dam/bmw/common/all-models/x-series/x6/2014/model-card/bmw-mperformance-x6-m50d-flyout-890x501.png");
+        }
+    }
+}
