157811c0c297cf7a958318e98d8f19b13b40e72e
User <User@WIN-GQOKRARBRRP>
2018-09-03 10:58:15 +0300

    small fix name of variable

--- Challenge 13-Download and Save Image/Program.cs
+++ Challenge 13-Download and Save Image/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main()
         {
-            ImageDownloader neDownloader = new ImageDownloader();
-            neDownloader.SaveImage(@"https://www.bmw.ua/content/dam/bmw/common/all-models/x-series/x6/2014/model-card/bmw-mperformance-x6-m50d-flyout-890x501.png");
+            ImageDownloader newDownloader = new ImageDownloader();
+            newDownloader.SaveImage(@"https://www.bmw.ua/content/dam/bmw/common/all-models/x-series/x6/2014/model-card/bmw-mperformance-x6-m50d-flyout-890x501.png");
         }
     }
 }
